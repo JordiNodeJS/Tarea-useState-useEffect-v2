bd9552790554c215cc1620990ff8fae4d5defc8e
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-12-06 21:27:12 +0100

    styling

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,13 +1,7 @@
 const Circle = () => {
   return (
     <svg height="100" width="100">
-      <circle
-        cx="50"
-        cy="50"
-        r="45"
-        stroke="black"
-        fill="red"
-      />
+      <circle cx="50" cy="50" r="45" stroke="black" fill="red" />
       Circle.
     </svg>
   )
