886b8578578fb1775f4c0b6f48bc91666085e897
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-12-07 14:29:25 +0100

    avoid negative numbers

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
     triangleSize: 100,
     triangleColor: '#b4008d'
   })
- const [alert, setAlert] = useState('')
   const {
     circleSize,
     circleColor,
@@ -24,52 +23,56 @@ function App() {
 
   useEffect(() => {
     if (circleSize === squareSize && squareSize === triangleSize)
-      setAlert('same size')
+      alert('same size')
     if (circleColor === squareColor && squareColor === triangleColor)
-      setAlert('same color')
+      alert('same color')
   }, [geometry])
 
   return (
     <div>
-      <div className="alert">{alert}</div>
       <Circle size={circleSize} color={circleColor} />
       <input
         type="number"
-        onChange={({ target: { value } }) =>
+        onChange={({ target: { value } }) => {
+          value = +value < 1 ? 100 : +value
           setGeometry(prev => ({ ...prev, circleSize: value }))
-        }
+        }}
         value={circleSize}
       />
       <input
         type="color"
-        onChange={({ target: { value } }) =>
+        onChange={({ target: { value } }) => {
+          value = +value < 1 ? 100 : +value
           setGeometry(prev => ({ ...prev, circleColor: value }))
-        }
+        }}
         value={circleColor}
       />
       <hr />
       <Square size={squareSize} color={squareColor} />
       <input
         type="number"
-        onChange={({ target: { value } }) =>
+        onChange={({ target: { value } }) => {
+          value = +value < 1 ? 100 : +value
           setGeometry(prev => ({ ...prev, squareSize: value }))
-        }
+        }}
         value={squareSize}
       />
       <input
         type="color"
-        onChange={({ target: { value } }) =>
+        onChange={({ target: { value } }) => {
+          value = +value < 1 ? 100 : +value
           setGeometry(prev => ({ ...prev, squareColor: value }))
-        }
+        }}
         value={squareColor}
       />
       <hr />
       <Triangle size={triangleSize} color={triangleColor} />
       <input
         type="number"
-        onChange={({ target: { value } }) =>
+        onChange={({ target: { value } }) => {
+          value = +value < 1 ? 100 : +value
           setGeometry(prev => ({ ...prev, triangleSize: value }))
-        }
+        }}
         value={triangleSize}
       />
       <input
